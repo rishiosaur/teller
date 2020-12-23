b2b3c9a701116e1ad4cfbbd51834e6032ca0a4ef
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-23 09:56:12 -0500

    index

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
-import { signing_secret, token, name } from './config';
-import { App } from "@slack/bolt";
-import { filterDM, filterNoBotMessages, filterChannel } from './middleware/index';
+import { signing_secret, token, name } from './config'
+import { App } from '@slack/bolt'
+import {
+	filterDM,
+	filterNoBotMessages,
+	filterChannel,
+} from './middleware/index'
 import * as features from './features/index'
 
-const app = new App({
-    signingSecret: signing_secret,
-    token: token
-});
+export const app = new App({
+	signingSecret: signing_secret,
+	token: token,
+})
 
-(async () => {
-      // Start your app
-  await app.start(process.env.PORT || 3000);
+;(async () => {
+	// Start your app
+	await app.start(process.env.PORT || 3000)
 
-  console.log(`${name} is running! 🔥`);
+	console.log(`${name} is running! 🔥`)
 
-  for (const [ feature, handler ] of Object.entries(features)) {
-      handler(app);
-      console.log(`Feature "${feature}" has been loaded.`)
-  }
-  
+	for (const [feature, handler] of Object.entries(features)) {
+		handler(app)
+		console.log(`Feature "${feature}" has been loaded.`)
+	}
 })()
