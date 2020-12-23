754e0a05139ed651e153e8eab1069d3e4948c807
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-23 09:55:46 -0500

    user functions util

---
+++ src/functions/users.ts
@@ -0,0 +1,39 @@
+import { client } from './graphql'
+import { gql } from 'graphql-request'
+
+export const isUser = (user: string) => !!user.match(/<@(.+?)>/)
+
+export const unwrapUser = (user: string) =>
+	user.match(/<@(.+?)\|(.+?)>/)[1] || null
+
+export const userExists = async (id: string) => {
+	let exists = false
+	await client
+		.request(
+			gql`
+				query Query($id: String!) {
+					user(id: $id) {
+						id
+					}
+				}
+			`,
+			{ id }
+		)
+		.then(() => (exists = true))
+		.catch(() => (exists = false)) // Throws error if DB doesn't exist
+
+	return exists
+}
+
+export const createUser = async (user: string) =>
+	client.request(
+		gql`
+			mutation CreateUser($user: String!) {
+				createUser(id: $user) {
+					id
+					secret
+				}
+			}
+		`,
+		{ user }
+	)
