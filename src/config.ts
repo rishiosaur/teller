c8fbdca4d67f897e45736ae5a545eb6c0a03b2a5
Rishi Kothari <itsrishikothari@gmail.com>
2020-12-23 09:56:16 -0500

    configs

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
-export const signing_secret = process.env.signing_secret;
-export const token = process.env.token;
-export const name = "Hello World!"
+export const signing_secret = process.env.signing_secret
+export const token = process.env.token
+export const name = 'The Teller'
+export const adminToken = process.env.admin
+export const apiURL = process.env.API_URL
