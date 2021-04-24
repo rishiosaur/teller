9118ac364e3fea07d7a45904bd98b6ed4b928f43
Rishi Kothari <itsrishikothari@gmail.com>
2021-04-23 20:28:04 -0400

    add hnchannel config

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,4 @@ export const { token } = process.env
 export const name = 'The Teller'
 export const adminToken = process.env.admin
 export const apiURL = process.env.API_URL
+export const HNChannel = process.env.channel
